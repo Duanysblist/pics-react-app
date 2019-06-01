89af2ff527fb6d280e5b03bb8305803f396986f5
Daniel Duany <dduany919@gmail.com>
2019-05-31 23:53:15 -0400

    Added unsplash API to search for photos

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import SearchBar from './SearchBar';
+import axios from 'axios';
 
 class App extends React.Component {
   onSearchSubmit(term) {
-    console.log(term);
+    axios.get('https://api.unspash.com/search/photos', {
+      params: { query: term },
+      headers: {
+        Authorization:
+          'Client-ID be5d0a31ccdf1e4665775f47495eebca5cac8ab6dfdd23791eb63b867155d147'
+      }
+    });
   }
 
   render() {
